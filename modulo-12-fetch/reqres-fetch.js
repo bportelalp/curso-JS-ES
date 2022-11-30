8fb304bd056542fa4d1f713d0871bd9732af5879
bportela <bportela.dev@gmail.com>
2022-11-30 17:20:58 +0100

    fetch con error 404

--- modulo-12-fetch/reqres-fetch.js
+++ modulo-12-fetch/reqres-fetch.js
@@ -1,13 +1,23 @@
 
 
-let getUsers = () => {
-    fetch('https://reqres.in/api/users',{mode: 'cors'})
-    .then(r => {
-        let json = r.json();
-        console.log('Se ha obtenido el json: ',json);
-        return json;
-    })
-    .then(users  => {
-        console.log('Se recupera el primer usuario: ', users.data[0].first_name);
-    });
+let getUsers = (page) => {
+    let url = 'https://reqres.in/api/users';
+    if(typeof page == 'number')
+        url = url + '/' + 23;
+    fetch(url, { mode: 'cors' })
+        .then(r => {
+            if (r.ok) {
+                let json = r.json();
+                console.log('Se ha obtenido el json: ', json);
+                return json;
+            }
+            else {
+                console.log('No fue correcta la solicitud');
+                return Promise.reject('Error code: ' + r.status);
+            }
+        })
+        .then(users => {
+            console.log('Se recupera el primer usuario: ', users.data[0].first_name);
+        })
+        .catch(err => console.error(err));
 }
