e4443c1f117be0fea6c2f1ddeffd18a6d2bfd3e6
bportela <bportela.dev@gmail.com>
2022-12-09 13:27:13 +0100

    generadores como objetos e iterar sobre ellos

--- modulo-18-iteradores/iterador-fibonacci.js
+++ modulo-18-iteradores/iterador-fibonacci.js
@@ -1,3 +1,5 @@
+
+// Como iterador
 let fibonacci = {
     [Symbol.iterator]() {
         let pre = 0, cur = 1;
@@ -10,6 +12,7 @@ let fibonacci = {
     }
 }
 
+// Como iterador con generador
 let fibonacciWithGen = {
     [Symbol.iterator]: function* () {
         let pre = 0, cur = 1;
@@ -20,9 +23,19 @@ let fibonacciWithGen = {
     }
 }
 
-const generateSeries = (max) => {
+// Como generador como objeto, en realidad ahora basta con llamar a fibo() para obtenerla
+var fibo = function*() {
+    let pre = 0, cur = 1;
+    for (;;) {
+        [pre, cur] = [cur, pre + cur];
+        yield cur;
+    }
+};
+
+
+const generateSeries = (max, iterator) => {
     let fibonacciSeries = [];
-    for (var n of fibonacciWithGen) {
+    for (var n of iterator) {
         if (n > max)
             break;
         fibonacciSeries.push(n);
@@ -30,5 +43,7 @@ const generateSeries = (max) => {
     return fibonacciSeries;
 }
 
-let serie5000 = generateSeries(5000);
+// let serie5000 = generateSeries(5000, fibonacci);
+// let serie5000 = generateSeries(5000, fibonacciWithGen);
+let serie5000 = generateSeries(5000, fibo())
 console.log(serie5000);
