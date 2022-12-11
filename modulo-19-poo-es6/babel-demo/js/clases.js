423be5ae680f94fcf7409f0033b232abfd5eb484
bportela <bportela.dev@gmail.com>
2022-12-11 13:05:09 +0100

    Depurar con el sourcemap a partir del código original

--- modulo-19-poo-es6/babel-demo/js/clases.js
+++ modulo-19-poo-es6/babel-demo/js/clases.js
@@ -1,10 +1,12 @@
 class Foo {
-    constructor(x, y){
+    constructor(x, y) {
         this.x = x;
         this.y = y;
     }
+
+    DoStuff() {
+        console.log(this.x, this.y);
+    }
 }
 
-let f = new Foo(10,20);
-
-console.log(f.x, f.y);
+let f = new Foo(10, 20);
--- modulo-19-poo-es6/babel-demo/lib-es5/clases.js
+++ modulo-19-poo-es6/babel-demo/lib-es5/clases.js
@@ -1,14 +1,28 @@
 "use strict";
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Foo = function Foo(x, y) {
-    _classCallCheck(this, Foo);
+var Foo = function () {
+    function Foo(x, y) {
+        _classCallCheck(this, Foo);
 
-    this.x = x;
-    this.y = y;
-};
+        this.x = x;
+        this.y = y;
+    }
+
+    _createClass(Foo, [{
+        key: "DoStuff",
+        value: function DoStuff() {
+            console.log(this.x, this.y);
+        }
+    }]);
+
+    return Foo;
+}();
 
 var f = new Foo(10, 20);
 
-console.log(f.x, f.y);
+var something = function something() {};
+//# sourceMappingURL=clases.js.map
